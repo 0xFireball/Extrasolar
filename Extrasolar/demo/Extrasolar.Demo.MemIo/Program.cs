8ac99cb85e26c1a898afd4d9e9c544e12f183e69
0xFireball <0xFireball@outlook.com>
2017-01-18 21:20:43 -0800

    use barrier for synchronization

--- Extrasolar/demo/Extrasolar.Demo.MemIo/Program.cs
+++ Extrasolar/demo/Extrasolar.Demo.MemIo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Extrasolar.JsonRpc;
 using Extrasolar.JsonRpc.Types;
+using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Extrasolar.Demo.MemIo
@@ -8,18 +10,20 @@ namespace Extrasolar.Demo.MemIo
     public class Program
     {
         private static MemoryStream _basicIoStream = new MemoryStream();
+        private static Barrier _ioClientsReady = new Barrier(2);
 
         public static void Main()
         {
             // Test basic MemIO JSON RPC
             Task.Factory.StartNew(BasicMemIoClient1);
             Task.Factory.StartNew(BasicMemIoClient2);
-            Task.Yield().GetAwaiter().GetResult();
+            Task.Delay(-1).GetAwaiter().GetResult();
         }
 
         private static async Task BasicMemIoClient2()
         {
             var rpcClient = new JsonRpcClient(_basicIoStream, JsonRpcClient.ClientMode.TwoWay);
+            _ioClientsReady.SignalAndWait();
             await rpcClient.SendRequest(new Request("ping", null, "0"));
         }
 
@@ -30,10 +34,12 @@ namespace Extrasolar.Demo.MemIo
             {
                 if (!req.IsNotification)
                 {
+                    Console.WriteLine($"Client called method ${req.Method}.");
                     return new ResultResponse(req, null);
                 }
                 return null;
             });
+            _ioClientsReady.SignalAndWait();
         }
     }
 }
