e78efe52243b6ab78a35b290a0a837d13ca3b950
0xFireball <0xFireball@outlook.com>
2017-01-18 20:41:10 -0800

    add simple test project

---
+++ Extrasolar/demo/Extrasolar.Demo.MemIo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Extrasolar.Demo.MemIo
+{
+    public class Program
+    {
+        private static MemoryStream _basicIoStream = new MemoryStream();
+        private static StreamWriter _basicIoWriter = new StreamWriter(_basicIoStream);
+        private static StreamReader _basicIoReader = new StreamReader(_basicIoStream);
+
+        public static void Main(string[] args)
+        {
+            // Test basic MemIO JSON RPC
+            Task.Factory.StartNew(BasicMemIoClient1);
+            Task.Factory.StartNew(BasicMemIoClient2);
+        }
+
+        private static void BasicMemIoClient2()
+        {
+            var rpcClient = new JsonRpcClient();
+        }
+
+        private static void BasicMemIoClient1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
