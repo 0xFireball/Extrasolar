c36212017c7b62b3292f3e166c6956e47f0faf33
0xFireball <0xFireball@outlook.com>
2017-01-18 20:30:56 -0800

    add handler loop

--- Extrasolar/src/Extrasolar/JsonRpc/JsonRpcClient.cs
+++ Extrasolar/src/Extrasolar/JsonRpc/JsonRpcClient.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using Extrasolar.JsonRpc.Types;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Extrasolar.JsonRpc
 {
@@ -8,11 +12,50 @@ namespace Extrasolar.JsonRpc
         public StreamWriter DataWriter { get; private set; }
         public StreamReader DataReader { get; private set; }
 
-        public JsonRpcClient(Stream transportStream)
+        public bool Listening { get; set; } = true;
+
+        [Flags]
+        public enum ClientMode
+        {
+            Request,
+            Response
+        }
+
+        public ClientMode Mode { get; }
+
+        public JsonRpcClient(Stream transportStream, ClientMode clientMode = ClientMode.Request | ClientMode.Response)
         {
             TransportStream = transportStream;
             DataWriter = new StreamWriter(TransportStream);
             DataReader = new StreamReader(TransportStream);
+            Mode = clientMode;
+            if (Mode.HasFlag(ClientMode.Response))
+            {
+                Task.Factory.StartNew(ReceiveDataEventLoop);
+            }
         }
+
+        public async Task SendRequest(Request request)
+        {
+            await DataWriter.WriteLineAsync(request.ToString());
+            await DataWriter.FlushAsync();
+        }
+
+        public async Task ReceiveDataEventLoop()
+        {
+            var requestJson = await DataReader.ReadLineAsync();
+            var request = JsonConvert.DeserializeObject<Request>(requestJson);
+            // Spawn new handler
+            var handlerTask = Task.Factory.StartNew(async () => await HandleRequest(request));
+        }
+
+        private async Task HandleRequest(Request request)
+        {
+            var response = RequestHandler?.Invoke(request);
+            await DataWriter.WriteAsync(response.ToString());
+            await DataWriter.FlushAsync();
+        }
+
+        public Func<Request, Response> RequestHandler { get; set; }
     }
 }
