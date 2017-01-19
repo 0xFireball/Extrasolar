159a7a382ae89ba2221dc64ad0db595487d133f7
0xFireball <0xFireball@outlook.com>
2017-01-18 20:00:58 -0800

    add jsonrpc protocol types

---
+++ Extrasolar/src/Extrasolar/JsonRpc/Types/Request.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Extrasolar.JsonRpc.Types
+{
+    public class Request
+    {
+        /// <summary>
+        /// JSON-RPC protocol version
+        /// </summary>
+        [JsonProperty("jsonrpc", Required = Required.Always)]
+        public string JsonRpc => "2.0";
+
+        /// <summary>Unique request identifier.</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id { get; private set; }
+
+        /// <summary>The name of the remote method.</summary>
+        [JsonProperty("method", Required = Required.Always)]
+        public string Method { get; private set; }
+
+        /// <summary>Optional parameters.</summary>
+        [JsonProperty("params")]
+        public JToken Parameters { get; private set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ Extrasolar/src/Extrasolar/JsonRpc/Types/Response.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Extrasolar.JsonRpc.Types
+{
+    public class Response
+    {
+        [JsonProperty("jsonrpc", Required = Required.Always)]
+        public string Version;
+
+        /// <summary>Unique request identifier.</summary>
+        [JsonProperty("id", Required = Required.Always)]
+        public int Id;
+
+        /// <summary>The result if no error occured.</summary>
+        [JsonProperty("result")]
+        public JToken Result;
+
+        /// <summary>The error. null if no error occured.</summary>
+        [JsonProperty("error")]
+        public Error Error;
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
