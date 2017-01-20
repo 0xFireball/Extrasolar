e8238222664e11d4ba94ffb733a9a54532e7a98c
0xFireball <0xFireball@outlook.com>
2017-01-20 00:03:58 -0800

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+#set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+# Dotnet info
+
+dotnet --version
+
+echo "Restoring packages..."
+# Restore packages
+dotnet restore
+
+echo "Building project..."
+
+# Build
+dotnet build -c Release Extrasolar/src/Extrasolar
+
+echo "Running tests..."
+
+# Run tests
+dotnet test Extrasolar/test/Extrasolar.Tests
+
+echo "Publishing project..."
+
+# Publish
+dotnet publish -c Release Extrasolar/src/Extrasolar
