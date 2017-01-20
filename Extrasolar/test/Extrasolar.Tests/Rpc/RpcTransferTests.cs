80eb773a561f71818aaaf01f57f5e30da5aaed71
0xFireball <0xFireball@outlook.com>
2017-01-20 12:59:07 -0800

    add tests for passing objects

--- Extrasolar/test/Extrasolar.Tests/Rpc/RpcTransferTests.cs
+++ Extrasolar/test/Extrasolar.Tests/Rpc/RpcTransferTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using Extrasolar.Tests.Types;
+using Xunit;
 
 namespace Extrasolar.Tests.Rpc
 {
@@ -26,6 +27,17 @@ namespace Extrasolar.Tests.Rpc
         [Fact]
         public void CanCallSimpleObjects()
         {
+            var c1 = new TastyCookie(TastyCookie.CookieFlavor.Chocolate, 6, 1);
+            var vol = c1.Radius * c1.Radius * c1.Thickness;
+            Assert.Equal(_fixture.Client.GetVolume(c1), vol);
+        }
+
+        [Fact]
+        public void CanCallMultipleSimpleObjects()
+        {
+            var c1 = new TastyCookie(TastyCookie.CookieFlavor.Chocolate, 6, 1);
+            var c2 = new TastyCookie(TastyCookie.CookieFlavor.Chocolate, 8, 1.2);
+            Assert.Equal(_fixture.Client.Compare(c1, c2), false);
         }
     }
 }
