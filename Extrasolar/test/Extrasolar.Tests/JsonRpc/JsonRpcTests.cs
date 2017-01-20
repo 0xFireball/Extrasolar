ad8238d040cee61ed466c65d4390acec261dddfd
0xFireball <0xFireball@outlook.com>
2017-01-19 20:42:36 -0800

    add some tests

---
+++ Extrasolar/test/Extrasolar.Tests/JsonRpc/JsonRpcTests.cs
@@ -0,0 +1,46 @@
+﻿using Extrasolar.JsonRpc;
+using Extrasolar.JsonRpc.Types;
+using Extrasolar.Tests.Mocks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Extrasolar.Tests.JsonRpc
+{
+    public class JsonRpcTests
+    {
+        [Fact]
+        public void IsDisposable()
+        {
+            using (var rpcEndpoint = new JsonRpcEndpoint(new UnlimitedMemoryStream(), JsonRpcEndpoint.EndpointMode.Client))
+            {
+                // Do some work
+                Assert.IsAssignableFrom(typeof(IDisposable), rpcEndpoint);
+            }
+        }
+
+        [Fact]
+        public async Task CanCommunicate()
+        {
+            var commStream = new UnlimitedMemoryStream();
+            using (var client = new JsonRpcEndpoint(commStream, JsonRpcEndpoint.EndpointMode.Client))
+            {
+                using (var server = new JsonRpcEndpoint(commStream, JsonRpcEndpoint.EndpointMode.Server))
+                {
+                    string pong = "pong";
+                    server.RequestPipeline.AddItemToEnd((req) =>
+                    {
+                        return new ResultResponse(req, pong);
+                    });
+                    client.ResponsePipeline.AddItemToEnd((res) =>
+                    {
+                        Assert.Equal((res.Result as JValue).Value, pong);
+                        return true;
+                    });
+                    await client.SendRequest(new Request("ping", null, "0"));
+                }
+            }
+        }
+    }
+}
---
+++ Extrasolar/test/Extrasolar.Tests/Mocks/MemoryTransportLayer.cs
@@ -0,0 +1,15 @@
+﻿using Extrasolar.IO.Transport;
+using System.IO;
+
+namespace Extrasolar.Tests.Mocks
+{
+    public class MemoryTransportLayer : ITransportLayer
+    {
+        private UnlimitedMemoryStream _strm = new UnlimitedMemoryStream();
+
+        public Stream GetStream()
+        {
+            return _strm;
+        }
+    }
+}
