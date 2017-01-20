0669371e36663a5d6577a433f8716c1ea8243e3e
0xFireball <0xFireball@outlook.com>
2017-01-19 20:32:29 -0800

    add memorystream wrapper

---
+++ Extrasolar/test/Extrasolar.Tests/Mocks/UnlimitedMemoryStream.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Extrasolar.Tests.Mocks
+{
+    public class UnlimitedMemoryStream : Stream
+    {
+        private MemoryStream _memStrm = new MemoryStream();
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => true;
+
+        public override long Length => _memStrm.Length;
+
+        public override long Position
+        {
+            get
+            {
+                return _memStrm.Position;
+            }
+
+            set
+            {
+                _memStrm.Position = value;
+            }
+        }
+
+        public override void Flush()
+        {
+            _memStrm.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _memStrm.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _memStrm.Write(buffer, offset, count);
+        }
+    }
+}
