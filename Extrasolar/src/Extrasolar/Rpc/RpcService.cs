d6252211fe3157a77d6ee71ce409bfbfe923da76
0xFireball <0xFireball@outlook.com>
2017-01-20 19:56:25 -0800

    send jsonrpc error codes on failure

--- Extrasolar/src/Extrasolar/Rpc/RpcService.cs
+++ Extrasolar/src/Extrasolar/Rpc/RpcService.cs
@@ -23,116 +23,131 @@ namespace Extrasolar.Rpc
 
         private Response HandleRequest(Request request)
         {
-            // Handle request
-            // First bind to service method
-            var methodName = request.Method;
-            // Process arguments
-            var rawArgs = request.Parameters;
-            var callArgs = new List<object>();
-            var rawParams = ((JValue)rawArgs).Value;
-            if (rawParams is string)
+            try
             {
-                var paramsData = JsonConvert.DeserializeObject<object>((string)rawParams);
-                if (paramsData is JArray)
+                // Handle request
+                // First bind to service method
+                var methodName = request.Method;
+                // Process arguments
+                var rawArgs = request.Parameters;
+                var callArgs = new List<object>();
+                var rawParams = ((JValue)rawArgs).Value;
+                if (rawParams is string)
                 {
-                    // TODO: Properly deserialize values
-                    //var paramsArray = (paramsData as JArray).Children().Select(x => x.ToObject<object>());
-                    var paramsArray = JsonConvert.DeserializeObject<object[]>((string)rawParams);
-                    foreach (var parameter in paramsArray)
+                    var paramsData = JsonConvert.DeserializeObject<object>((string)rawParams);
+                    if (paramsData is JArray)
                     {
-                        callArgs.Add(parameter);
-                    }
-                }
-                else
-                {
-                    callArgs.Add(paramsData);
-                }
-            }
-            // TODO: Get proper object args
-            var targetMethodCandidates = _cachedMethodInfo.Where(x => x.Name == methodName);
-            // Check if we have a definitive target
-            if (!targetMethodCandidates.Any())
-            {
-                // No implementation found
-                throw new NotImplementedException();
-            }
-            if (targetMethodCandidates.Count() > 1)
-            {
-                // Attempt to resolve by checking parameter count and types
-                var paramCount = callArgs.Count;
-                targetMethodCandidates = targetMethodCandidates.Where(x => x.GetParameters().Count() == paramCount);
-                targetMethodCandidates = targetMethodCandidates.Where(method =>
-                {
-                    var prmTypes = method.GetParameters().Select(x => x.ParameterType).ToArray();
-                    var tmpCallArgs = new List<object>(callArgs);
-                    for (int i = 0; i < prmTypes.Length; i++)
-                    {
-                        var paramType = prmTypes[i];
-                        var callParam = tmpCallArgs[i];
-                        if (!paramType.IsInstanceOfType(callParam))
+                        // TODO: Properly deserialize values
+                        //var paramsArray = (paramsData as JArray).Children().Select(x => x.ToObject<object>());
+                        var paramsArray = JsonConvert.DeserializeObject<object[]>((string)rawParams);
+                        foreach (var parameter in paramsArray)
                         {
-                            // If the call parameter isn't an instance, cast
-                            var originalCallArgType = callParam.GetType();
-                            // Only convert long to int for calls
-                            //tmpCallArgs[i] = Convert.ChangeType(callParam, paramType);
-                            if (paramType == typeof(int) && callParam is long)
-                            {
-                                tmpCallArgs[i] = Convert.ChangeType(callParam, typeof(int));
-                            }
-                            // If that succeeded, types are convertible.
-                            // Now make sure it's assignable
-                            if (!paramType.IsAssignableFrom(tmpCallArgs[i].GetType()))
-                            {
-                                // Not assignable
-                                return false;
-                            }
-                            // Finally, use a type constraint
-                            if (paramType.FullName != tmpCallArgs[i].GetType().FullName)
-                            {
-                                // Type does not exactly match
-                                return false;
-                            }
+                            callArgs.Add(parameter);
                         }
                     }
-                    return true;
-                });
+                    else
+                    {
+                        callArgs.Add(paramsData);
+                    }
+                }
+                // TODO: Get proper object args
+                var targetMethodCandidates = _cachedMethodInfo.Where(x => x.Name == methodName);
+                // Check if we have a definitive target
                 if (!targetMethodCandidates.Any())
                 {
                     // No implementation found
                     throw new NotImplementedException();
                 }
-                else if (targetMethodCandidates.Count() > 1)
+                if (targetMethodCandidates.Count() > 1)
                 {
-                    // TODO: Check all types
-                    if (targetMethodCandidates.Count() > 1)
+                    // Attempt to resolve by checking parameter count and types
+                    var paramCount = callArgs.Count;
+                    targetMethodCandidates = targetMethodCandidates.Where(x => x.GetParameters().Count() == paramCount);
+                    targetMethodCandidates = targetMethodCandidates.Where(method =>
                     {
-                        // Could not resolve method definitively
-                        throw new ArgumentException("Could not resolve method definitively");
+                        var prmTypes = method.GetParameters().Select(x => x.ParameterType).ToArray();
+                        var tmpCallArgs = new List<object>(callArgs);
+                        for (int i = 0; i < prmTypes.Length; i++)
+                        {
+                            var paramType = prmTypes[i];
+                            var callParam = tmpCallArgs[i];
+                            if (!paramType.IsInstanceOfType(callParam))
+                            {
+                                // If the call parameter isn't an instance, cast
+                                var originalCallArgType = callParam.GetType();
+                                // Only convert long to int for calls
+                                //tmpCallArgs[i] = Convert.ChangeType(callParam, paramType);
+                                if (paramType == typeof(int) && callParam is long)
+                                {
+                                    tmpCallArgs[i] = Convert.ChangeType(callParam, typeof(int));
+                                }
+                                // If that succeeded, types are convertible.
+                                // Now make sure it's assignable
+                                if (!paramType.IsAssignableFrom(tmpCallArgs[i].GetType()))
+                                {
+                                    // Not assignable
+                                    return false;
+                                }
+                                // Finally, use a type constraint
+                                if (paramType.FullName != tmpCallArgs[i].GetType().FullName)
+                                {
+                                    // Type does not exactly match
+                                    return false;
+                                }
+                            }
+                        }
+                        return true;
+                    });
+                    if (!targetMethodCandidates.Any())
+                    {
+                        // No implementation found
+                        throw new NotImplementedException();
+                    }
+                    else if (targetMethodCandidates.Count() > 1)
+                    {
+                        // TODO: Check all types
+                        if (targetMethodCandidates.Count() > 1)
+                        {
+                            // Could not resolve method definitively
+                            return new ErrorResponse(request, new Error(Error.JsonRpcErrorCode.MethodNotFound, "Could not resolve method definitively", methodName));
+                        }
                     }
                 }
-            }
-            var selectedMethod = targetMethodCandidates.First();
-            // Normalize arguments
-            var parameterTypes = selectedMethod.GetParameters().Select(x => x.ParameterType).ToArray();
-            for (int i = 0; i < parameterTypes.Length; i++)
-            {
-                var paramType = parameterTypes[i];
-                var callParam = callArgs[i];
-                if (!paramType.IsInstanceOfType(callParam) || paramType == typeof(object))
+                var selectedMethod = targetMethodCandidates.First();
+                // Normalize arguments
+                var parameterTypes = selectedMethod.GetParameters().Select(x => x.ParameterType).ToArray();
+                for (int i = 0; i < parameterTypes.Length; i++)
                 {
-                    // If the call parameter doesn't match, change type
-                    callArgs[i] = AutoConvert(callParam, paramType);
+                    var paramType = parameterTypes[i];
+                    var callParam = callArgs[i];
+                    if (!paramType.IsInstanceOfType(callParam) || paramType == typeof(object))
+                    {
+                        // If the call parameter doesn't match, change type
+                        callArgs[i] = AutoConvert(callParam, paramType);
+                    }
+                }
+                try
+                {
+                    var result = selectedMethod.Invoke(ServiceImplementation, callArgs.ToArray());
+                    var resultJObject = JToken.FromObject(result);
+                    return new ResultResponse(request, resultJObject);
+                }
+                catch (Exception ex)
+                {
+                    return new ErrorResponse(request, new Error(Error.JsonRpcErrorCode.InternalError, ex.Message, JToken.FromObject(ex)));
                 }
             }
-            try
+            catch (InvalidCastException)
             {
-                var result = selectedMethod.Invoke(ServiceImplementation, callArgs.ToArray());
-                var resultJObject = JToken.FromObject(result);
-                return new ResultResponse(request, resultJObject);
+                return new ErrorResponse(request, new Error(Error.JsonRpcErrorCode.InvalidParams, "Could not call method with specified parameters", request.Method));
+            }
+            catch (NotImplementedException)
+            {
+                return new ErrorResponse(request, new Error(Error.JsonRpcErrorCode.MethodNotFound, "Could not find a matching method", request.Method));
             }
             catch (Exception ex)
             {
-                return new ErrorResponse(request, new Error(-1, ex.Message, ex.ToString()));
+                return new ErrorResponse(request, new Error(Error.JsonRpcErrorCode.InternalError, ex.Message, JToken.FromObject(ex)));
             }
         }
 
